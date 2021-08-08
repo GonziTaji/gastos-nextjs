8d78a4f4ab935b4dc8be731b4c2875534d49f9e4
gonzitachi <gonzalo.tajmuch@gmail.com>
2021-08-08 04:01:33 -0400

    feat: mongo_url with env usr and secret

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const { MONGO_USR, MONGO_SECRET } = process.env;
+
 module.exports = {
     env: {
-      // MONGO_URL: 'mongodb://localhost:27017',
+      MONGO_URL: 'mongodb+srv://'+MONGO_USR+':'+MONGO_SECRET+'@cluster0.jakm5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
       productionBrowserSourceMaps: true,
       reactStrictMode: true,
     },
