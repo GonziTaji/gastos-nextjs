a020f7e6b9902e4e0499b0beb612de1d42b87cfb
gonzitachi <gonzalo.tajmuch@gmail.com>
2021-08-08 03:37:09 -0400

    fix: removes mongo_url from env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
-      MONGO_URL: 'mongodb://localhost:27017',
+      // MONGO_URL: 'mongodb://localhost:27017',
       productionBrowserSourceMaps: true,
+      reactStrictMode: true,
     },
   }
